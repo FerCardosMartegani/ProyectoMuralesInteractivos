e10037afdd064e06582cc97e906040c8ca0682ca
FerCardosMartegani <103231341+FerCardosMartegani@users.noreply.github.com>
2024-07-20 19:13:52 -0300

    Add files via upload

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -4,20 +4,31 @@ let leaveNoteButton;
 let colorButtons = [];
 let previewBox;
 let isLeavingNote = false;
-let pastelColors = ['#FFB6C1', '#87CEFA', '#98FB98', '#FFD700', '#FFA07A', '#FF69B4', '#00FA9A', '#B0E0E6'];
+let pastelColors = [
+  "#FFB6C1",
+  "#87CEFA",
+  "#98FB98",
+  "#FFD700",
+  "#FFA07A",
+  "#FF69B4",
+  "#00FA9A",
+  "#B0E0E6",
+];
 let messages = [];
 let draggingNote = null;
 let enlargedNote = null;
 let customFont;
 let backgroundImage;
+let pinImage;
 let puedeGuardar = false;
 let tamanoXCanva = 370;
 let tamanoYcanva = 601;
-function preload() {
-  customFont = loadFont('../assets/DarumadropOne-Regular.ttf');
-  backgroundImage = loadImage('../assets/img/fondoCorcho.png');
-  backgroundImage2 = loadImage('../assets/img/fondoCorchoLargo.png');
 
+function preload() {
+  customFont = loadFont("../assets/DarumadropOne-Regular.ttf");
+  backgroundImage = loadImage("../assets/img/fondoCorcho.png");
+  backgroundImage2 = loadImage("../assets/img/fondoCorchoLargo.png");
+  pinImage = loadImage("../assets/img/pinchito.png");
 }
 
 function setup() {
@@ -27,13 +38,13 @@ function setup() {
   createInterface();
 
   // Load existing notes
-  existingNotes.forEach(note => {
+  existingNotes.forEach((note) => {
     messages.push(new Message(note.mensaje, note.posX, note.posY, note.color));
   });
 
-  let initialColor = color('#FFB6C1');
-  previewBox = new PreviewBox(width / 2 - 100, height / 2 - 100, initialColor);
-  previewBox.hide();
+  // let initialColor = color("#FFB6C1");
+  // previewBox = new PreviewBox(width / 2 - 100, height / 2 - 100, initialColor);
+  // previewBox.hide();
   textFont(customFont);
 }
 
@@ -55,35 +66,47 @@ function draw() {
   }
 
   if (draggingNote) {
-    draggingNote.updatePosition(mouseX - draggingNote.offsetX, mouseY - draggingNote.offsetY);
+    draggingNote.updatePosition(
+      mouseX - draggingNote.offsetX,
+      mouseY - draggingNote.offsetY
+    );
   }
 }
 
 function createInterface() {
-  leaveNoteButton = createButton('¡Dejar Nota!');
+  leaveNoteButton = document.getElementById("botonDejarNota");
 
-  leaveNoteButton.style(`
-    background-color: #FF8181;
-    color: black;
-    padding: 10px 20px;
-    border: 1px solid;
-    border-radius: 20px;
-    cursor: pointer;
-    font-family: "Darumadrop One", sans-serif;
-    font-size: 16px;
-  `);
-  leaveNoteButton.mousePressed(() => {
+  // leaveNoteButton = createButton('¡Dejar Nota!');
+
+  // leaveNoteButton.style(`
+  //   background-color: #FF8181;
+  //   color: black;
+  //   padding: 10px 20px;
+  //   border: 1px solid;
+  //   border-radius: 20px;
+  //   cursor: pointer;
+  //   font-family: "Darumadrop One", sans-serif;
+  //   font-size: 16px;
+  // `);
+  // leaveNoteButton.mousePressed(() => {
+  //   isLeavingNote = !isLeavingNote;
+  //   toggleInterface(isLeavingNote);
+  //   if (isLeavingNote) {
+  //     showPreview();
+  //   }
+  // });
+  leaveNoteButton.onclick = () => {
     isLeavingNote = !isLeavingNote;
     toggleInterface(isLeavingNote);
     if (isLeavingNote) {
       showPreview();
     }
-  });
+  };
 
   pastelColors.forEach((color, i) => {
-    let colorButton = createButton('');
+    let colorButton = createButton("");
     colorButton.size(30, 30);
-    colorButton.style('background-color', color);
+    colorButton.style("background-color", color);
     colorButton.mousePressed(() => {
       previewBox.updateColor(color);
     });
@@ -93,7 +116,7 @@ function createInterface() {
   input = createInput();
   input.input(showPreview);
 
-  button = createButton('Enviar');
+  button = createButton("Enviar");
   button.mousePressed(addMessage);
   button.style(`
     background-color: #FF8181;
@@ -110,68 +133,119 @@ function createInterface() {
 
   toggleInterface(false);
 
-  previewBox = new PreviewBox(width / 2 - 50, height / 2 - 50, color(255, 0, 0));
+  // previewBox = new PreviewBox(
+  //   width / 2 - 50,
+  //   height / 2 - 50,
+  //   color(255, 0, 0)
+  // );
+  // previewBox.hide();
+
+  let initialColor = color("#FFB6C1");
+  previewBox = new PreviewBox(
+    width / 2 - 100,
+    (height * 1) / 3 - 100,
+    initialColor
+  );
   previewBox.hide();
 }
 
 function adjustInterfacePositions() {
-  if (windowWidth >= 600) {   
-    //escritorio
-    tamanoXCanva=1000;
-    tamanoYcanva=600;
-    resizeCanvas(tamanoXCanva, tamanoYcanva);
+  // if (windowWidth <= 395) {
+  //   tamanoXCanva = 330;
+  //   tamanoYcanva = 550;
+  //   resizeCanvas(tamanoXCanva, tamanoYcanva);
 
-    leaveNoteButton.position(tamanoXCanva/1.78, height +tamanoYcanva/16.66);
+  //   // leaveNoteButton.position(
+  //   //   tamanoXCanva / 2.75,
+  //   //   height + tamanoYcanva / 10.66
+  //   // );
 
-    colorButtons.forEach((button, i) => {
-      button.position(tamanoXCanva/1.83 + i * 30, height - tamanoYcanva/3.75);
-    });
+  //   colorButtons.forEach((button, i) => {
+  //     button.position(tamanoXCanva / 4.75 + i * 30, height - tamanoYcanva / 4);
+  //   });
 
-    input.position(tamanoXCanva/1.75, height -  tamanoYcanva/5);
+  //   input.position(tamanoXCanva / 3.5, height - tamanoYcanva / 5.5);
 
-    button.position(tamanoXCanva/1.63, height -  tamanoYcanva/6.6);
-  } else { // mobile
-    leaveNoteButton.position(150, 700);
+  //   button.position(tamanoXCanva / 2.25, height - tamanoYcanva / 8);
+  // } else if (windowWidth >= 395 && windowWidth <= 490) {
+  //   tamanoXCanva = 370;
+  //   tamanoYcanva = 750;
 
-    colorButtons.forEach((button, i) => {
-      button.position(100 + i * 30, 460);
-    });
+  //   // leaveNoteButton.position(tamanoXCanva / 2.5, height + tamanoYcanva / 16.66);
 
-    input.position(80, 515);
+  //   colorButtons.forEach((button, i) => {
+  //     button.position(tamanoXCanva / 4 + i * 30, height - tamanoYcanva / 4.75);
+  //   });
 
-    button.position(input.x + input.width + 10, input.y-10);
-  }
+  //   input.position(tamanoXCanva / 3.1, height - tamanoYcanva / 6.75);
+
+  //   button.position(tamanoXCanva / 2.25, height - tamanoYcanva / 9.75);
+  // } else if (windowWidth > 490) {
+  //   tamanoXCanva = 900;
+  //   tamanoYcanva = 600;
+  //   // leaveNoteButton.position(600, 650);
+  //   resizeCanvas(tamanoXCanva, tamanoYcanva);
+
+  //   colorButtons.forEach((button, i) => {
+  //     button.position(575 + i * 30, 460);
+  //   });
+
+  //   input.position(575, 515);
+
+  //   button.position(input.x + input.width + 10, input.y - 10);
+  // }
+  tamanoXCanva = windowWidth * 0.9;
+  tamanoYcanva = map(tamanoXCanva, 900, 370, 600, 750);
+  tamanoYcanva = constrain(
+    tamanoYcanva,
+    (windowHeight * 2) / 5,
+    (windowHeight * 4) / 5
+  );
+
+  resizeCanvas(tamanoXCanva, tamanoYcanva);
+
+  let newX = width / 2 - (colorButtons.length / 2) * 30;
+  let newY = height * 0.75;
+
+  colorButtons.forEach((button, i) => {
+    button.position(newX + i * 30, newY);
+  });
+
+  input.position(newX, newY + 55);
+
+  button.position(input.x + input.width + 10, input.y - 10);
 }
 
 function toggleInterface(show) {
   if (show) {
     input.show();
     button.show();
-    colorButtons.forEach(button => button.show());
+    colorButtons.forEach((button) => button.show());
   } else {
     input.hide();
     button.hide();
-    colorButtons.forEach(button => button.hide());
+    colorButtons.forEach((button) => button.hide());
   }
 }
 
 function showPreview() {
   previewBox.show();
   previewBox.updateText(input.value());
-  leaveNoteButton.hide();
+  // leaveNoteButton.hide();
+  leaveNoteButton.style.display = "none";
 }
 
 function addMessage() {
   let inputText = input.value();
   let x = random(width - 50);
   let y = random(height - 50);
-  let color = previewBox.color.toString('#rrggbb');
+  let color = previewBox.color.toString("#rrggbb");
 
   let newMessage = new Message(inputText, x, y, color);
-  document.getElementById('botonGenerarMuro').style.display = 'block';
+  document.getElementById("botonCrearMuro").style.display = "block";
 
   messages.push(newMessage);
-  input.value('');
+  input.value("");
   if (isLeavingNote) {
     previewBox.hide();
     toggleInterface(false);
@@ -179,23 +253,35 @@ function addMessage() {
 
   input.hide();
   button.hide();
-  colorButtons.forEach(button => button.hide());
-  leaveNoteButton.hide();
+  colorButtons.forEach((button) => button.hide());
+  // leaveNoteButton.hide();
 
-  document.getElementById('falseInputNotita').value = inputText;
-  document.getElementById('falseInputPosX').value = x;
-  document.getElementById('falseInputPosY').value = y;
-  document.getElementById('falseInputColor').value = color;
+  document.getElementById("falseInputNotita").value = inputText;
+  document.getElementById("falseInputPosX").value = x;
+  document.getElementById("falseInputPosY").value = y;
+  document.getElementById("falseInputColor").value = color;
 
   $.ajax({
     type: "POST",
     url: "guardarNota.php",
     data: $("#falseForm").serialize(),
-    success: function(response) {
-      console.log('Nota guardada:', response);
-    }
+    success: function (response) {
+      console.log("Nota guardada:", response);
+    },
   });
 }
+
+function doubleClicked() {
+  for (let message of messages) {
+    if (message.isMouseInside()) {
+      enlargedNote = message;
+      return;
+    }
+  }
+  enlargedNote = null;
+}
+
+let isDragging = false; // Nueva variable
 
 function mousePressed() {
   for (let message of messages) {
@@ -203,46 +289,83 @@ function mousePressed() {
       draggingNote = message;
       message.offsetX = mouseX - message.x;
       message.offsetY = mouseY - message.y;
+      isDragging = true; // Indica que se está arrastrando
       return;
     }
   }
 }
 
-function doubleClicked() {
-  if (enlargedNote) {
-    enlargedNote = null; // Cerrar la nota agrandada
-  }
-}
-
 function mouseReleased() {
   if (draggingNote) {
-    if (!enlargedNote) { // Evitar que se actualice la posición cuando se está haciendo doble clic para agrandar una nota
-      $.ajax({
-        type: "POST",
-        url: "actualizarNota.php", // URL para actualizar la posición de la nota en el servidor
-        data: {
-          mensaje: draggingNote.text, // Utilizamos el mensaje como identificador
-          posX: draggingNote.x,
-          posY: draggingNote.y,
-          color: draggingNote.color 
-        },
-        success: function(response) {
-          console.log('Posición de la nota actualizada:', response);
-        }
-      });
-    }
+    $.ajax({
+      type: "POST",
+      url: "actualizarNota.php",
+      data: {
+        mensaje: draggingNote.text,
+        posX: draggingNote.x,
+        posY: draggingNote.y,
+        color: draggingNote.color,
+      },
+      success: function (response) {
+        console.log("Posición de la nota actualizada:", response);
+      },
+    });
   }
   draggingNote = null;
+  isDragging = false; // Resetea al soltar
 }
 
 function mouseClicked() {
-  if (enlargedNote && enlargedNote.isMouseInside()) {
-    enlargedNote = null; // Cerrar la nota agrandada al hacer clic sobre ella
-  } else {
+  if (!isDragging) {
+    // Solo ejecuta si no se está arrastrando
+    if (enlargedNote && enlargedNote.isMouseInside()) {
+      enlargedNote = null;
+    } else {
+      for (let message of messages) {
+        if (message.isMouseInside() && windowWidth < 490) {
+          enlargedNote = message;
+          return;
+        }
+      }
+    }
+  }
+}
+
+function mouseDragged() {
+  if (!isDragging && messages.length >= 2) {
+    let mouseMovedX;
+    let mouseMovedY;
+    let notePosesX = [];
+    let notePosesY = [];
+
+    mouseMovedX = mouseX - pmouseX;
+    mouseMovedY = mouseY - pmouseY;
+
     for (let message of messages) {
-      if (message.isMouseInside()) {
-        enlargedNote = message; // Mostrar la nota agrandada
-        return;
+      notePosesX.push(message.x);
+      notePosesY.push(message.y);
+    }
+    notePosesX.sort();
+    notePosesY.sort();
+    notePosesY.reverse();
+
+    console.log(notePosesY);
+    console.log(notePosesY[notePosesY.length - 1]);
+
+    let canGoLeft = notePosesX[0] > 50 && mouseX < pmouseX;
+    let canGoRight =
+      notePosesX[notePosesX.length - 1] + 100 < width - 50 && mouseX > pmouseX;
+    if (canGoLeft || canGoRight) {
+      for (let message of messages) {
+        message.x += mouseMovedX;
+      }
+    }
+    let canGoUp = notePosesY[0] > 50 && mouseY < pmouseY;
+    let canGoDown =
+      notePosesY[notePosesY.length - 1] + 100 < height - 50 && mouseY > pmouseY;
+    if (canGoUp || canGoDown) {
+      for (let message of messages) {
+        message.y += mouseMovedY;
       }
     }
   }
@@ -265,16 +388,22 @@ class Message {
     if (enlarged) {
       fill(this.color);
       noStroke();
-      rect(this.x, this.y, 200, 200); // Ajustar el tamaño de la nota agrandada
+      rect(this.x, this.y, 200, 200);
+      if (draggingNote !== this) {
+        image(pinImage, this.x + 90, this.y, 20, 20);
+      }
       fill(0);
       noStroke();
       textAlign(CENTER, CENTER);
       textSize(24);
-      text(this.text, this.x + 100, this.y + 100); // Centrar el texto en la nota agrandada
+      text(this.text, this.x + 100, this.y + 100);
     } else {
       fill(this.color);
       noStroke();
       rect(this.x, this.y, this.width, this.height);
+      if (draggingNote !== this) {
+        image(pinImage, this.x + this.width / 2 - 10, this.y, 20, 20);
+      }
       fill(0);
       noStroke();
       textAlign(CENTER, CENTER);
@@ -284,7 +413,12 @@ class Message {
   }
 
   isMouseInside() {
-    return mouseX > this.x && mouseX < this.x + this.width && mouseY > this.y && mouseY < this.y + this.height;
+    return (
+      mouseX > this.x &&
+      mouseX < this.x + this.width &&
+      mouseY > this.y &&
+      mouseY < this.y + this.height
+    );
   }
 
   updatePosition(x, y) {
@@ -322,11 +456,14 @@ class PreviewBox {
 
   display() {
     if (this.visible) {
-      fill(0,100);
-      rect(0,0,windowWidth,windowHeight);
+      fill(0, 100);
+      rect(0, 0, windowWidth, windowHeight);
       fill(this.color);
       noStroke();
       rect(this.x, this.y, this.width, this.height);
+      if (!isLeavingNote) {
+        image(pinImage, this.x + this.width / 2 - 10, this.y, 20, 20);
+      }
       fill(0);
       noStroke();
       textAlign(CENTER, CENTER);
